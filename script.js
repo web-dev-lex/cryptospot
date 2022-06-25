c913ad86f5597d47094b48983b77320fe0e6ec89
Lex <webdevlex35@gmail.com>
2022-06-24 23:42:22 -0400

    update, working on javascript to add dropdown on elements

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function dropDown() {
+  let dropdown = document.querySelector('#btc-dropdown');
+  dropdown.style.display = "flex";
+  
+}
+
+let btcBtn = document.getElementById('btc-btn');
+btcBtn.addEventListener("click", dropDown);
+
+
+ 
