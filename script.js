089057d1eb136c0e845544f66b70448535903d89
Lex <webdevlex35@gmail.com>
2022-06-26 11:26:36 -0400

    update live price & color change on button click

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
 
  
+// a true/false statement to be used in conjuction with functions
 
 let value = true;
+
+// functions for dropdown info
 
 function btcDropDown() {
   let btcDropinfo = document.querySelector('.btc-dropdown');
@@ -90,39 +93,50 @@ function dogeDropDown() {
 
 function dotDropDown() {
   let dotDropinfo = document.querySelector('.dot-dropdown');
+  let dotBtn = document.querySelector('.dot-btn');
 
   if (value === true) {
     dotDropinfo.style.display = "flex";
+    dotBtn.style.cssText = "background-color: #f9f9f9; color: #000;";
     value = false;
   } else {
     dotDropinfo.style.display = "none";
+    dotBtn.style.cssText = "background-color: #3b4260; color: #fff;"
     value = true;
   }
 }
 
 function shibDropDown() {
   let shibDropinfo = document.querySelector('.shib-dropdown');
+  let shibBtn = document.querySelector('.shib-btn');
 
   if (value === true) {
     shibDropinfo.style.display = "flex";
+    shibBtn.style.cssText = "background-color: #f9f9f9; color: #000;";
     value = false;
   } else {
     shibDropinfo.style.display = "none";
+    shibBtn.style.cssText = "background-color: #3b4260; color: #fff;"
     value = true;
   }
 }
 
 function tronDropDown() {
   let tronDropinfo = document.querySelector('.tron-dropdown');
+  let tronBtn = document.querySelector('.tron-btn');
 
   if (value === true) {
     tronDropinfo.style.display = "flex";
+    tronBtn.style.cssText = "background-color: #f9f9f9; color: #000;";
     value = false;
   } else {
     tronDropinfo.style.display = "none";
+    tronBtn.style.cssText = "background-color: #3b4260; color: #fff;";
     value = true;
   }
 }
+
+// event listeners for each drop down info button
 
 let btcBtn = document.querySelector('.btc-btn');
 btcBtn.addEventListener("click", btcDropDown);
@@ -153,3 +167,95 @@ shibBtn.addEventListener("click", shibDropDown);
 
 let tronBtn = document.querySelector('.tron-btn');
 tronBtn.addEventListener("click", tronDropDown);
+
+// websockets for live price data
+
+let ws1 = new WebSocket('wss://stream.binance.us:9443/ws/btcusd@trade');
+let btcPriceElement = document.getElementById('btc-price');
+
+ws1.onmessage = (event) => {
+  let btcObject = JSON.parse(event.data);
+  btcPriceElement.innerText = `$${parseFloat(btcObject.p).toFixed(2)}`;
+}
+
+
+let ws2 = new WebSocket('wss://stream.binance.us:9443/ws/ethusd@trade');
+let ethPriceElement = document.getElementById('eth-price');
+
+ws2.onmessage = (event) => {
+  let ethObject = JSON.parse(event.data);
+  ethPriceElement.innerText = `$${parseFloat(ethObject.p).toFixed(2)}`;
+}
+
+
+let ws3 = new WebSocket('wss://stream.binance.us:9443/ws/bnbusd@trade');
+let bnbPriceElement = document.getElementById('bnb-price');
+
+ws3.onmessage = (event) => {
+  let bnbObject = JSON.parse(event.data);
+  bnbPriceElement.innerText = `$${parseFloat(bnbObject.p).toFixed(2)}`;
+}
+
+
+let ws4 = new WebSocket('wss://stream.binance.us:9443/ws/xrpusd@trade');
+let xrpPriceElement = document.getElementById('xrp-price');
+
+ws4.onmessage = (event) => {
+  let xrpObject = JSON.parse(event.data);
+  xrpPriceElement.innerText = `$${parseFloat(xrpObject.p).toFixed(3)}`;
+}
+
+
+let ws5 = new WebSocket('wss://stream.binance.us:9443/ws/adausd@trade');
+let adaPriceElement = document.getElementById('ada-price');
+
+ws5.onmessage = (event) => {
+  let adaObject = JSON.parse(event.data);
+  adaPriceElement.innerText = `$${parseFloat(adaObject.p).toFixed(4)}`;
+}
+
+
+let ws6 = new WebSocket('wss://stream.binance.us:9443/ws/solusd@trade');
+let solPriceElement = document.getElementById('sol-price');
+
+ws6.onmessage = (event) => {
+  let solObject = JSON.parse(event.data);
+  solPriceElement.innerText = `$${parseFloat(solObject.p).toFixed(2)}`;
+}
+
+
+let ws7 = new WebSocket('wss://stream.binance.us:9443/ws/dogeusd@trade');
+let dogePriceElement = document.getElementById('doge-price');
+
+ws7.onmessage = (event) => {
+  let dogeObject = JSON.parse(event.data);
+  dogePriceElement.innerText = `$${parseFloat(dogeObject.p).toFixed(3)}`;
+}
+
+
+let ws8 = new WebSocket('wss://stream.binance.us:9443/ws/dotusd@trade');
+let dotPriceElement = document.getElementById('dot-price');
+
+ws8.onmessage = (event) => {
+  let dotObject = JSON.parse(event.data);
+  dotPriceElement.innerText = `$${parseFloat(dotObject.p).toFixed(2)}`;
+}
+
+
+let ws9 = new WebSocket('wss://stream.binance.us:9443/ws/shibusd@trade');
+let shibPriceElement = document.getElementById('shib-price');
+
+ws9.onmessage = (event) => {
+  let shibObject = JSON.parse(event.data);
+  shibPriceElement.innerText = `$${parseFloat(shibObject.p).toFixed(6)}`;
+}
+
+
+let ws10 = new WebSocket('wss://stream.binance.us:9443/ws/trxusd@trade');
+let trxPriceElement = document.getElementById('trx-price');
+
+ws10.onmessage = (event) => {
+  let trxObject = JSON.parse(event.data);
+  trxPriceElement.innerText = `$${parseFloat(trxObject.p).toFixed(3)}`;
+}
+
